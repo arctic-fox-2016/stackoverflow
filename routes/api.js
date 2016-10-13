9f2fd2b54ec9babba3fbdff1978d3d969b066c05
Tev <email": null>
2016-10-13 21:11:45 +0700

    install npm and set public source

---
+++ routes/api.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var author = require('../controller/author')
+var blogpost = require('../controller/blogposts')
+var comment = require('../controller/comments')
+var question = require('../controller/question')
+var user = require('../controller/user')
+
+/* GET users listing. */
+
+//question
+router.get('/question', question.find_all_question)
+router.get('/question/:question_id', question.find_question)
+router.put('/question/:question_id', question.update_question)
+router.post('/question/:question_id/upvotes/:user_id', question.questin_upvotes)
+router.post('/question/:question_id/downvotes/:user_id', question.question_downvotes)
+router.post('/question', question.insert_question)
+router.delete('/question/:question_id', question.delete_question)
+
+//answer
+router.post('/question/:question_id/answer/:user_id', question.insert_answer)
+router.post('/question/:question_id/answer/:user_id/upvotes', question.answer_upvotes)
+router.post('/question/:question_id/answer/:user_id/downvotes', question.answer_downvotes)
+router.post('/question/:question_id/answer/:user_id/update/:answer_num', question.update_answer)
+router.delete('/question/:question_id/answer/:answer_num', question.delete_answer)
+
+//user
+router.get('/user', user.find_all_user)
+router.get('/user/:user_id', user.find_user)
+router.put('/user/:user_id', user.update_user)
+router.post('/user', user.insert_user)
+router.delete('/user/:user_id', user.delete_user)
+
+module.exports = router;
